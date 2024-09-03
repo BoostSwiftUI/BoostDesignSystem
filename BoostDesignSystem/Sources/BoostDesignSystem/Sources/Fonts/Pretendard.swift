13631c2bcbf79658cd26e3d2b58ff85ab8f948a5
SeungHyun Hong <whi7ehyun@gmail.com>
2024-09-03 20:06:57 +0900

    del: 불필요한 개행 삭제

--- BoostDesignSystem/Sources/BoostDesignSystem/Sources/Fonts/Pretendard.swift
+++ BoostDesignSystem/Sources/BoostDesignSystem/Sources/Fonts/Pretendard.swift
@@ -9,17 +9,12 @@
 
 public enum Pretendard: String, CaseIterable {
   case black = "Pretendard-Black"
-
   case extraBold = "Pretendard-ExtraBold"
   case bold = "Pretendard-Bold"
   case semiBold = "Pretendard-SemiBold"
-
   case medium = "Pretendard-Medium"
-
   case regular = "Pretendard-Regular"
-
   case light = "Pretendard-Light"
   case extraLight = "Pretendard-ExtraLight"
-
   case thin = "Pretendard-Thin"
 }
