7a1fd129abb36b522c0e02f71b25f019bd9a3aa7
SeungHyun Hong <whi7ehyun@gmail.com>
2024-09-03 15:27:19 +0900

    add: BoostFont, View+Font extension 추가

---
+++ BoostDesignSystem/Sources/BoostDesignSystem/Sources/Extensions/View+Font.swift
@@ -0,0 +1,14 @@
+//
+//  View+Font.swift
+//
+//
+//  Created by Hyun on 9/3/24.
+//
+
+import SwiftUI
+
+public extension View {
+  func font(_ style: BoostFont) -> some View {
+    self
+  }
+}
---
+++ BoostDesignSystem/Sources/BoostDesignSystem/Sources/Fonts/BoostFont.swift
@@ -0,0 +1,31 @@
+//
+//  BoostFont.swift
+//
+//
+//  Created by Hyun on 9/3/24.
+//
+
+import SwiftUI
+
+public enum BoostFont {
+  case heading1(Weight)
+  case heading2(Weight)
+  case heading3(Weight)
+  case subheading1(Weight)
+  case subheading2(Weight)
+  case body1(Weight)
+  case body2(Weight)
+  case caption1(WeightWithSize)
+
+  public enum Weight {
+    case bold
+    case medium
+    case regular
+  }
+
+  public enum WeightWithSize {
+    case bold(Int)
+    case medium(Int)
+    case regular(Int)
+  }
+}
