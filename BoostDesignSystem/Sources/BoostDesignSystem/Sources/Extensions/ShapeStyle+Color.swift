d00b0c2e8a5e21f6425c26aa7b6507dde8d4d20c
SeungHyun Hong <whi7ehyun@gmail.com>
2024-09-06 08:59:03 +0900

    fix: 색상 가져오지 못하는 버그 수정
    - 에셋이 폴더링되어있어 Gray Scale을 접두사에 추가

--- BoostDesignSystem/Sources/BoostDesignSystem/Sources/Extensions/ShapeStyle+Color.swift
+++ BoostDesignSystem/Sources/BoostDesignSystem/Sources/Extensions/ShapeStyle+Color.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 public extension ShapeStyle where Self == Color {
-  static var gray50: Color { .init("gray50", bundle: .module) }
-  static var gray100: Color { .init("gray100", bundle: .module) }
-  static var gray200: Color { .init("gray200", bundle: .module) }
-  static var gray300: Color { .init("gray300", bundle: .module) }
-  static var gray400: Color { .init("gray400", bundle: .module) }
-  static var gray500: Color { .init("gray500", bundle: .module) }
-  static var gray600: Color { .init("gray600", bundle: .module) }
-  static var gray700: Color { .init("gray700", bundle: .module) }
-  static var gray800: Color { .init("gray800", bundle: .module) }
-  static var gray900: Color { .init("gray900", bundle: .module) }
+
+  // MARK: - Gray Scale
+
+  static var gray50: Color { .init("Gray Scale/gray50", bundle: .module) }
+  static var gray100: Color { .init("Gray Scale/gray100", bundle: .module) }
+  static var gray200: Color { .init("Gray Scale/gray200", bundle: .module) }
+  static var gray300: Color { .init("Gray Scale/gray300", bundle: .module) }
+  static var gray400: Color { .init("Gray Scale/gray400", bundle: .module) }
+  static var gray500: Color { .init("Gray Scale/gray500", bundle: .module) }
+  static var gray600: Color { .init("Gray Scale/gray600", bundle: .module) }
+  static var gray700: Color { .init("Gray Scale/gray700", bundle: .module) }
+  static var gray800: Color { .init("Gray Scale/gray800", bundle: .module) }
+  static var gray900: Color { .init("Gray Scale/gray900", bundle: .module) }
 }
